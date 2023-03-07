21d4df2297bf7d1b8922cca42044a9ce06e57d73
TingSyuanWang <vincent@fishboneapps.com>
2023-03-07 07:16:00 -0500

    feat: remove cocoapods and use swift package manager
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
--- Podfile
+++ Podfile
@@ -9,3 +9,15 @@ target 'Flash Chat iOS13' do
   pod 'FirebaseFirestore'
 
 end
+
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    if target.name == 'Realm'
+      create_symlink_phase = target.shell_script_build_phases.find { |x| x.name == 'Create Symlinks to Header Folders' }
+      create_symlink_phase.always_out_of_date = "1"
+    end
+    target.build_configurations.each do |config|
+      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '11.0'
+    end
+  end
+end
